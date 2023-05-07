a9567f20778a0a4ee4b1814435f70ec01d660b11
SantiEscudero14 <132808515+SantiEscudero14@users.noreply.github.com>
2023-05-07 20:51:09 -0300

    Add files via upload

---
+++ Ejercicio 3/ClaseRegistro.py
@@ -0,0 +1,21 @@
+class Registro:
+    
+    def __init__(self, temperatura: int, humedad: int, presion: float):
+        self.__temperatura = temperatura
+        self.__humedad = humedad
+        self.__presion = presion
+        
+    def get_temperatura(self):
+        return self.__temperatura
+    
+    def get_humedad(self):
+        return self.__humedad
+    
+    def get_presion(self):
+        return self.__presion
+    
+    def mostrar(self):
+        print("temperatura: ", self.__temperatura)
+        print("humedad: ", self.__humedad)
+        print("presion: ", self.__presion)
+    
